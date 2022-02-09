692f7017ddf588b1d18f09a209a552a798b15276
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-02-08 22:31:19 -0700

    concat fix

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
@@ -20,46 +11,70 @@ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var symbols = ["#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "|", "~"];
 
 //Generating random charachters
-var getRandomLower = lowerAlpha[Math.floor(Math.random() * lowerAlpha.length)];
-var getRandomUpper = upperAlpha[Math.floor(Math.random() * upperAlpha.length)];
-var getRandomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-var getRandomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-console.log(getRandomLower)
-console.log(getRandomUpper)
-console.log(getRandomNumber)
-console.log(getRandomSymbol)
+// var getRandomLower = lowerAlpha[Math.floor(Math.random() * lowerAlpha.length)];
+// var getRandomUpper = upperAlpha[Math.floor(Math.random() * upperAlpha.length)];
+// var getRandomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+// var getRandomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+// console.log(getRandomLower)
+// console.log(getRandomUpper)
+// console.log(getRandomNumber)
+// console.log(getRandomSymbol)
 
 //function to determine contents of the password
 function generatePassword() {
     window.alert("Hello! Please answer the following questions to generate your own personalized password.");
-    var charCountSelect = parseInt(window.prompt("How many charachters would you like in your password? Please select a number from 8-128."));
-    var lowerCaseSelect = window.prompt("Would you like to include lowercase letters? Enter YES or NO.");
-    var upperCaseSelect = window.prompt("Would you like to include upper case letters? Enter YES or NO.");
-    var numberSelect = window.prompt("Would you like to include numbers? Enter YES or NO.");
-    var symbolSelect = window.prompt("Would you like to include symbols? Enter YES or NO.");
+    var charCountSelect = parseInt(window.prompt("How many characters would you like in your password? Please select a number from 8-128."));
+        while(charCountSelect <= 7 || charCountSelect >= 129) {
+            window.alert("Please pick a number between 8-128.");
+            var charCountSelect = parseInt(window.prompt("How many characters would you like in your password? Please select a number from 8-128."));
+        }
+        
+        window.alert("Your password will have " + charCountSelect + " characters.");
+
+    var lowerCaseSelect = window.confirm("Would you like to include lowercase letters?");
+    console.log(lowerCaseSelect)
+    var upperCaseSelect = window.confirm("Would you like to include upper case letters?");
+    console.log(upperCaseSelect)
+    var numberSelect = window.confirm("Would you like to include numbers?");
+    console.log(numberSelect)
+    var symbolSelect = window.confirm("Would you like to include symbols?");
+    console.log(symbolSelect)
 
     var selectedCharacters = []
 
     if (lowerCaseSelect) {
-        selectedCharacters.concat(lowerAlpha)
+        selectedCharacters = selectedCharacters.concat(lowerAlpha)
     }
 
     if (upperCaseSelect) {
-        selectedCharacters.concat(upperAlpha)
+        selectedCharacters = selectedCharacters.concat(upperAlpha)
     }
 
     if (numberSelect) {
-        selectedCharacters.concat(numbers)
+        selectedCharacters = selectedCharacters.concat(numbers)
 
     }
 
     if (symbolSelect) {
-        selectedCharacters.concat(symbols)
+        selectedCharacters = selectedCharacters.concat(symbols)
     }
+    
+    console.log(selectedCharacters)
+
     var password = ""
+
     for (var i = 0; i < charCountSelect; i++) {
         var getRandomCharacter = selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
         password += getRandomCharacter
+        console.log(getRandomCharacter)
     }
     return password
 } 
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
